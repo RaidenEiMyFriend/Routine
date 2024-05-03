463f83e9ea85144a1dcbbb5fec3036a54fb26281
RaidenEiMyFriend <dinhvuha2113269@gmail.com>
2024-05-03 13:45:19 +0700

    push code

---
+++ views/main/about/index.php
@@ -0,0 +1,240 @@
+<?php
+   include_once('views/main/navbar.php');
+?>
+
+<link rel="stylesheet" href="assets/stylesheets/about.css">
+<section id="about-page">
+    <div class="container" style="margin-top: 80px">
+        <div class="row">
+            <div class="col-lg-6 d-flex justify-content-center">
+                <div class="d-flex flex-column align-items-center">
+                    <h1>Tại sao nên chọn Routine?</h1>
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <i class="fas fa-check-square fa-3x me-2"></i>
+                        <div>
+                            <h4> Tự tin khoe cá tính </h4>
+                            <p> Đến với chúng tôi, bạn sẽ được hiểu như thế nào là "thời trang phang thời tiết",
+                                với hơn 100 collections cho cả chàng và nàng, cho cả cái nóng giữa tháng 6 Hà Nội hay
+                                là cái lạnh tháng 12 trên đỉnh Sapa. </p>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <i class="fas fa-check-square fa-3x me-2"></i>
+                        <div>
+                            <h4> Muốn mua quà tặng người thương ư? Bạn đến đúng nơi rồi đó </h4>
+                            <p>Routine luôn hỗ trợ bạn với mọi mức giá, cho mọi đối tượng khách hàng:
+                                từ già trẻ, gái trai cho tới cao thấp, béo gầy. Với tiện ích gói quà miễn phí
+                                cho các đơn đặt hàng trong dịp cuối năm này, bạn còn chờ đợi gì nữa mà hãy tới bên 
+                                Routine.</p>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between">
+                        <i class="fas fa-check-square fa-3x me-2"></i>
+                        <div>
+                            <h4> Chất lượng đặt lên hàng đầu </h4>
+                            <p> Sản phẩm của Routine luôn đề cao tính thời thượng, nhưng trên hết
+                                chúng tôi hướng tới những sản phẩm bền bì với khách yêu, và chọn 
+                                chất liệu vải không gây khó chịu cho khách.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 d-flex justify-content-center">
+                <div class="card" style="width: 300px; height: 500px">
+                    <img src="https://images.squarespace-cdn.com/content/v1/5b8bf301e2ccd13e972a0ab4/1561964243970-9HBPQPM2GGRKOEMWVLNY/thiet-ke-cua-hang-routine-hcm%2B%2808%29.jpg"
+                        alt="Image" class="card-img-top">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container" style="margin-top: 80px">
+        <div class="row">
+            <div class="col-lg-6 d-flex justify-content-center">
+                <div class="card" style="width: 300px; height: 500px">
+                    <img src="https://images.squarespace-cdn.com/content/v1/5b8bf301e2ccd13e972a0ab4/1561964864841-MOC2MUKA0MNEZ9XKMZMO/thiet-ke-cua-hang-routine-ha-noi%2B%2806%29.jpg"
+                        alt="Image" class="card-img-top">
+                </div>
+            </div>
+            <div class="col-lg-6 d-flex justify-content-center">
+                <div class="d-flex flex-column align-items-center">
+                    <h1> Và còn rất nhiều điều chờ đón bạn khi bước tới Routine </h1>
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <i class="fas fa-check-square fa-3x me-2"></i>
+                        <div>
+                            <h4> Sale siêu khủng, quà siêu to </h4>
+                            <p>Nhân dịp cuối năm, Routine xin tặng các khách yêu những đợt sale có 
+                              một không hai. Đặc biệt khi tới các chi nhánh trên toàn quốc, bạn còn có
+                              cơ hội nhận về giải thưởng đặc biệt 1 chiếc Playstation, 5 giải nhì phiếu mua sắm
+                              miễn phí và hàng ngàn món quà hấp dẫn khác đang đợi bạn.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <i class="fas fa-check-square fa-3x me-2"></i>
+                        <div>
+                            <h4> Dịch vụ tuyệt vời </h4>
+                            <p> Các shop mở cửa từ 7:00 cho tới 23:00, từ thứ 2 cho tới Chủ Nhật.
+                                Vào thế giới của Routine, bạn sẽ được nhân viên tận tình giúp đỡ.
+                                Chúng tôi luôn luôn tuyển dụng những con người có phẩm chất tốt, đạo đức
+                                tốt.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between">
+                        <i class="fas fa-check-square fa-3x me-2"></i>
+                        <div>
+                            <h4>Cơ hội trở thành đại sứ</h4>
+                            <p> Bất kể bạn là ai, chỉ cần có đam mê và tự tin bạn luôn có thể
+                                trở thành người mẫu cho chúng tôi. Hãy đến Routine ngay hôm
+                                nay để trở thành người mẫu, và đảm bảo chúng tôi sẽ không
+                                làm bạn thất vọng với hàng loạt đãi ngộ và tiền công hậu hĩnh.
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
+
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="https://routine.vn/media/amasty/webp/catalog/product/cache/5b5632a96492396f42c72e22fdd64763/a/o/ao-nam-10f23shl090_light_blue_1__1_jpg.webp"
+                        class="card-img-top" alt="Image 1">
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="https://routine.vn/media/amasty/webp/catalog/product/cache/d0cf4470db45e8932c69fc124d711a7e/a/o/ao-thun-nam-13-10f23tss062_black_beauty_7__jpg.webp"
+                        class="card-img-top" alt="Image 2">
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="https://routine.vn/media/amasty/webp/catalog/product/cache/d0cf4470db45e8932c69fc124d711a7e/a/o/ao-thun-nam-13-10f23tss082_foxtrot_1__1_jpg.webp"
+                        class="card-img-top" alt="Image 3">
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="https://routine.vn/media/amasty/webp/catalog/product/cache/d0cf4470db45e8932c69fc124d711a7e/a/o/ao-thun-nam-13-10f23tss078_bright_white_1__1_jpg.webp"
+                        class="card-img-top" alt="Image 4">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-4">
+        <div class="d-flex justify-content-center">
+            <h3>Nhận xét của khách hàng</h3>
+        </div>
+        <div class="row align-items-start mt-4">
+            <div class="col-md-4">
+                <div class="card comment-card mx-auto">
+                    <div class="card-body">
+                        <h5 class="card-title">Lê Khánh An</h5>
+                        <p class="card-text">"Sản phẩm Routine chưa bao giờ
+                            làm mình thôi bất ngờ. Chất liệu xịn nè, mặc rất thoải mái 
+                            không rát vào mùa đông. Và dĩ nhiên là đẹp lắm "</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card comment-card mx-auto">
+                    <div class="card-body">
+                        <h5 class="card-title">Madara</h5>
+                        <p class="card-text">"Sau khi được Uế thố chuyển sinh xong ta đã bay ngay tới
+                            Routine để có thể lấy bộ giáp mơ ước của ta. Có nó, thế giới này sẽ trong tay ta"</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card comment-card mx-auto">
+                    <div class="card-body">
+                        <h5 class="card-title">Gojo Satoru</h5>
+                        <p class="card-text">"Có bán đồ cho 1 nửa cơ thể luôn hả ??? Chốt 10 đơn để mặc dưới âm phủ nhé shop"</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-center mt-5">
+            <h3>Đội ngũ phát triển trang web</h3>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <div class="about--info">
+        <div class="card--personal">
+            <div class="card--personal-info">
+                <img src="https://api.duniagames.co.id/api/content/upload/file/3472390091631072929.jpg" alt=""
+                    class="card-img">
+                <h2 class="name">Đinh Vũ Hà</h2>
+                <p class="role">Developer</p>
+                <div class="card-social">
+                    <a href="https://www.facebook.com/duchuy22902" class="facebook" class="facebook"><i
+                            class="fa-brands fa-facebook"></i></a>
+                    <a href="https://www.youtube.com/channel/UCmvIP_5mg49uGi5BSuRi9Nw" class="youtube"><i
+                            class="fa-brands fa-youtube"></i></a>
+                    <a href="https://github.com/duchuykg" class="github"><i class="fa-brands fa-github"></i></a>
+                </div>
+            </div>
+            <div class="card--personal-info">
+                <img src="https://api.duniagames.co.id/api/content/upload/file/8933243521633502531.png" alt=""
+                    class="card-img">
+                <h2 class="name">Nguyễn Hoài Khang</h2>
+                <p class="role">Developer</p>
+                <div class="card-social">
+                    <a href="#" class="facebook"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" class="youtube"><i class="fa-brands fa-youtube"></i></a>
+                    <a href="#" class="github"><i class="fa-brands fa-github"></i></a>
+                </div>
+            </div>
+            <div class="card--personal-info">
+                <img src="https://i.pinimg.com/originals/7e/ce/c4/7ecec434137d1fcbe023db38e06c1260.jpg" alt=""
+                    class="card-img">
+                <h2 class="name">Từ Mai Thế Nhân</h2>
+                <p class="role">Developer</p>
+                <div class="card-social">
+                    <a href="#" class="facebook"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" class="youtube"><i class="fa-brands fa-youtube"></i></a>
+                    <a href="#" class="github"><i class="fa-brands fa-github"></i></a>
+                </div>
+            </div>
+            <div class="card--personal-info">
+                <img src="https://media-assets-ggwp.s3.ap-southeast-1.amazonaws.com/2021/02/fakta-connie-springer-640x360.jpg"
+                    alt="" class="card-img">
+                <h2 class="name">Nguyễn Anh Khoa</h2>
+                <p class="role">Developer</p>
+                <div class="card-social">
+                    <a href="#" class="facebook"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" class="youtube"><i class="fa-brands fa-youtube"></i></a>
+                    <a href="#" class="github"><i class="fa-brands fa-github"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://kit.fontawesome.com/4ccf3877a2.js" crossorigin="anonymous"></script>
+</section>
+
+<?php
+   include_once('views/main/footer.php');
+?>
